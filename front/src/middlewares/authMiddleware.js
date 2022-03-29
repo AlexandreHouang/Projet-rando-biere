449c9ef286d25381aebba0d36b3a927bdfbdb3b7
AlexandreHouang <alexandre.houang@gmail.com>
2022-03-29 15:02:34 +0200

    front correction http

--- front/src/middlewares/authMiddleware.js
+++ front/src/middlewares/authMiddleware.js
@@ -7,7 +7,7 @@ import {
   deleteErrorMessage,
 } from 'src/actions/auth';
 
-const SERVER_URL = 'https://ec2-54-225-0-75.compute-1.amazonaws.com:3500/v1';
+const SERVER_URL = 'http://ec2-54-225-0-75.compute-1.amazonaws.com:3500/v1';
 //const SERVER_URL = 'http://ec2-3-85-243-18.compute-1.amazonaws.com:3500/v1';
 //const SERVER_URL = 'http://localhost:5555/v1';
 
